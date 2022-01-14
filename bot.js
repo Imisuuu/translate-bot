4691604d053cb6e0c6e72a85c074f5394c658a1a
Imisuuu <mikolaj.zyszkiewicz1@gmail.com>
2022-01-14 21:54:35 +0100

    version 1.0.0 ✨

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const translate = require('@vitalets/google-translate-api');
 client.config = require('./config');
 const ISO6391 = require('iso-639-1');
 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity('Simple bot that uses Google Translate API.');
@@ -23,12 +24,11 @@ client.on('messageCreate', (msg) => {
         var string = args.join(' ');
         try {
             translate(string, {to: client.config.lang}).then(res => {
-                console.log(args);
     
-                console.log(res.text);
                 //=> I speak English
-                msg.channel.send(res.text)
-                res.pronunciation
+                let from = ISO6391.getName(res.from.language.iso);
+                msg.channel.send(`**Translated from ${from}: **${res.text}`);
+
                 //=> nl
             }).catch(err => {
                 console.error(err);
@@ -45,7 +45,19 @@ client.on('messageCreate', (msg) => {
 `>>>Author of this bot is **Imisuuu**
 Github: https://github.com/Imisuuu`
 );
-      } 
+      } else if (command === 'help' || command === 'h'){
+        msg.channel.send(
+` ***         HELP ***
+>>> The prefix is **${client.config.prefix}**
+**Commands: **
+- translate (${client.config.prefix}t or ${client.config.prefix}translate)
+- getLanguage (${client.config.prefix}lang or ${client.config.prefix}getLanguage)
+- about (${client.config.prefix}about)
+- help (${client.config.prefix}h or ${client.config.prefix}help)
+
+The author is **${client.config.author}**
+`)
+      }
   }
 });
 
